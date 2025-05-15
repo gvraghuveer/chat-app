fffe7b4d2e3bff837dc75bc723d03b6108f604b3
gvraghuveer <78306776+gvraghuveer@users.noreply.github.com>
2025-05-15 07:16:42 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,8 +15,7 @@ const port = process.env.PORT || 5001;
 dotenv.config();
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
